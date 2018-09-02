fbf230d81ab44d1c5cbd3098b349ad08736aa986
Ondřej Hliba <hliba.ondra@live.com>
2018-09-02 21:14:51 +0200

    Add Testing For Async Add Expense Action Generators

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -12,7 +12,11 @@ console.log('before');
 
 promise.then((data) => {
     console.log('1', data);
-    return 'some data';
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('This is my other promise');
+        }, 5000);
+    });
 }).then((data) => {
     console.log('does this run?', data);
 }).catch((e) => {
