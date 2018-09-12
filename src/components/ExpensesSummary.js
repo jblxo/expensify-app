27af135976f1aa1939f7ff6cd67a737da53c87bf
Ondřej Hliba <hliba.ondra@live.com>
2018-09-12 15:18:59 +0200

    Add Styles For Page Header And ExpensesListFilters

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,16 +3,22 @@ import { connect } from 'react-redux';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
+import { Link } from 'react-router-dom';
 
 export const ExpensesSummary = ({expensesTotal, expenseCount}) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     
     return (
-        <div>
-            <h1>
-                Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}
-            </h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">
+                    Viewing <span>{expenseCount} {expenseWord}</span> totalling <span>{formattedExpensesTotal}</span>
+                </h1>
+                <div className="page-header__actions">
+                    <Link className="button button--blue" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
