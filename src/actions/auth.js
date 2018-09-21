c18cb00ca944fd08ba4e56d5330086c7a685d8b6
Ondřej Hliba <hliba.ondra@live.com>
2018-09-21 11:29:36 +0200

    Add Facebook Login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,13 +1,13 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase';
+import { firebase } from '../firebase/firebase';
 
 export const login = (uid) => ({
     type: 'LOGIN',
     uid
 });
 
-export const startLogin = () => {
+export const startLogin = (provider) => {
     return () => {
-        return firebase.auth().signInWithPopup(googleAuthProvider);
+        return firebase.auth().signInWithPopup(provider);
     };
 };
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
+import { googleAuthProvider, facebookAuthProvider } from '../firebase/firebase';
 
-export const LoginPage = ({ startLogin }) => (
-    <div className={"box-layout"}>
-        <div className={"box-layout__box"}>
-            <h1 className={"box-layout__title"}>Expensify</h1>
-            <p>It's time to get your expenses under control.</p>
-            <button className={"button button--blue"} onClick={startLogin}>Login with Google</button>
-        </div>
-    </div>
-);
+export class LoginPage extends React.Component {
+    googleLogin = () => {
+        this.props.startLogin(googleAuthProvider)
+    };
+
+    facebookLogin = () => {
+        this.props.startLogin(facebookAuthProvider)
+    }
+
+    render() {
+        return (
+            <div className={"box-layout"}>
+                <div className={"box-layout__box"}>
+                    <h1 className={"box-layout__title"}>Expensify</h1>
+                    <p>It's time to get your expenses under control.</p>
+                    <button className={"button button--blue"} onClick={this.googleLogin}>Login with Google</button>
+                    <button className={"button button--blue"} onClick={this.facebookLogin}>Login with Facebook</button>
+                </div>
+            </div>
+        )
+    }
+}
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => { dispatch(startLogin()) }
+    startLogin: (provider) => { dispatch(startLogin(provider)) }
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
